3084a2dc5d945af5c59614fd7e60ff0d7e22faaa
= <=>
2017-01-28 09:49:24 -0600

    Write parsed data to CSV file

--- parse.py
+++ parse.py
@@ -3,18 +3,21 @@ import sys
 import time
 import urllib2
 import xml.etree.ElementTree as ET
+import csv
 
 tree = ET.parse('stream.xml')
 root = tree.getroot()
 
-#for i in range(1,100,1):
-#	z = root.getchildren()[2].getchildren()[0].getchildren()[3].getchildren()[i]
-#	print z.getchildren()[0].text, "|", z.getchildren()[1].text
-
 namespace = "http://www.dummy-temp-address"
 xpath = './/{{{0}}}collection-periods/{{{0}}}collection-period/{{{0}}}detector-reports/{{{0}}}detector-report'.format(namespace)
-print xpath
 reports = tree.findall(xpath)
 
+csvfile = open('streamdata.csv', 'wb')
+csv = csv.writer(csvfile)
+
+csv.writerow(['Detector-ID', 'Status'])
+
 for i in range(1,100,1):
-    print reports[i].getchildren()[0].text, "|", reports[i].getchildren()[1].text
+    csv.writerow([reports[i].getchildren()[0].text, reports[i].getchildren()[1].text])
+
+csvfile.close()
